d5bd40ded0910a84568eace30824af31e205837c
ParadoxXO <ParadoxXO@DESKTOP-OEQN22I>
2025-04-16 16:06:39 +0300

    directive tamamlandi

--- Directive/directive/src/app/app.component.ts
+++ Directive/directive/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RenklendirDirective } from './renklendir.directive';
+import { RouterOutlet } from '@angular/router';
+import { ValidateDirective } from './validate.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RenklendirDirective],
+  imports: [RenklendirDirective, RouterOutlet, ValidateDirective],
   template: `
-  <h1 renklendir = "green" test ="test">Directive</h1>
+  <h1>Directive</h1>
+  <form>
+    <input id="name" autocomplete="off" validate type="text" required minlength="3">
+    <div></div>
+    <button type="submit">Send</button>
+  </form>
   `
 })
 export class AppComponent {
+
 }
