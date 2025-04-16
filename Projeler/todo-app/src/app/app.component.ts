e19912448be291a7ba802b4b4325c126d2037e4c
ParadoxXO <ParadoxXO@DESKTOP-OEQN22I>
2025-04-16 11:30:36 +0300

    kredi hesaplama yapildi

--- Projeler/todo-app/src/app/app.component.ts
+++ Projeler/todo-app/src/app/app.component.ts
@@ -6,130 +6,70 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   template: `
-    <style>
-      .container {
-        max-width: 500px;
-        margin: 50px auto;
-        padding: 20px;
-        border: 2px solid #ccc;
-        border-radius: 12px;
-        box-shadow: 0 4px 8px rgba(0,0,0,0.1);
-        font-family: Arial, sans-serif;
-        background-color: #f9f9f9;
-      }
-      h1 {
-        text-align: center;
-        color: #333;
-      }
-      div {
-        margin-bottom: 20px;
-      }
-      label {
-        display: block;
-        margin-bottom: 5px;
-        font-weight: bold;
-      }
-      input {
-        width: 100%;
-        padding: 8px;
-        margin-bottom: 5px;
-        border: 1px solid #aaa;
-        border-radius: 6px;
-      }
-      button {
-        margin-right: 10px;
-        padding: 6px 12px;
-        border: none;
-        border-radius: 6px;
-        background-color: #007bff;
-        color: white;
-        cursor: pointer;
-      }
-      button:hover {
-        background-color: #0056b3;
-      }
-      ul {
-        list-style: none;
-        padding: 0;
-      }
-      li {
-        margin-bottom: 10px;
-        background-color: #fff;
-        padding: 10px;
-        border-radius: 8px;
-        box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-      }
-    </style>
-
-    <div class="container">
-      <h1>Todo App</h1>
-      @if(!isUpdateWorkFormActive ){
-        <div>
-        <label>Work</label>
-        <input [(ngModel)]="work">
-        <button (click)="save()">Save</button>
-      </div>
-      }@else {
-        <div>
-        <label>Update Work</label>
-        <input [(ngModel)]="updateWork">
-        <button (click)="update()">Update</button>
-      </div>
-      }
-
-
-      <hr>
-
-      <div>
-        <ul>
-          @for (data of todos; track data) {
-            <li>
-              {{data}}
-              <span>
-                @if(!isUpdateWorkFormActive){
-                  <button (click)="delete($index)">Delete</button>
-                  <button (click)="get($index)">Update</button>
-                }
-              </span>
-            </li>
-          }
-        </ul>
-      </div>
+    <h1>Kredi Hesaplama</h1>
+    <div>
+      <label>Kredi Tutarı</label>
+      <input [(ngModel)]="krediTutari">
     </div>
+    <div>
+      <label> Taksit Sayısı </label>
+      <select [(ngModel)]="taksitSayisi">
+        @for(data of taksitler; track data){
+          <option>{{data}}</option>
+        }
+      </select>
+    </div>
+    <div>
+      <button (click)="hesapla()">Hesapla</button>
+      <label >{{this.result}}</label>
+    </div>
+    <hr>
+    <table>
+      <thead>
+        <tr>
+          <th>Taksit</th>
+          <th>Taksit Tutarı</th>
+          <th>Kalan Geri Ödeme</th>
+        </tr>
+      </thead>
+      <tbody>
+        @for (data of odemePlani; track data) {
+          <tr>
+            <td>{{$index+1}}</td>
+            <td>{{data.taksitTutari}}</td>
+            <td>{{data.kalanGeriOdeme}}</td>
+          </tr>
+
+        }
+      </tbody>
+    </table>
   `
 })
 export class AppComponent {
-  work: string = "";
-  updateWork: string = "";
-  updateIndex: number = 0;
-  todos: string[] = [];
-  isUpdateWorkFormActive: boolean = false;
+  krediTutari: number = 0;
+  taksitSayisi: number = 3;
+  taksitler: number[] = [3, 6, 9, 12, 24];
 
-  save() {
-    if (this.work.trim()) {
-      this.todos.push(this.work.trim());
-      this.work = "";
+  result: string = ""
+
+  odemePlani: { taksitTutari: number, kalanGeriOdeme: number }[] = []
+
+  hesapla() {
+    const taksitTutari = (this.krediTutari / this.taksitSayisi) * 1.29
+    let toplamGeriOdeme = taksitTutari * this.taksitSayisi
+
+    this.result = `Taksit Tutarı : ${taksitTutari} - Taksit Sayısı : ${this.taksitSayisi} - Toplam Geri Ödeme: ${toplamGeriOdeme}`
+
+    this.odemePlani = [];
+    for (let i = 0; i < this.taksitSayisi; i++) {
+      toplamGeriOdeme -= taksitTutari
+      const data = {
+        taksitTutari: taksitTutari,
+        kalanGeriOdeme: toplamGeriOdeme
+      }
+      this.odemePlani.push(data);
     }
-  }
 
-  delete(index: number) {
-    this.todos.splice(index, 1);
-  }
-
-  get(index: number) {
-    this.updateIndex = index;
-    this.updateWork = this.todos[index];
-    this.isUpdateWorkFormActive =true;
-  }
-
-  update() {
-    if (this.updateWork.trim()) {
-      this.todos[this.updateIndex] = this.updateWork.trim();
-      this.updateWork = "";
-      this.isUpdateWorkFormActive =false;
-    }
   }
 }
+
